e6560ea4c2e44a6bec3bf8c2a5403f8e3c5cca8d
youness <youness.mounaime2002@gmail.com>
2023-03-16 12:41:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	/* C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line. */
+	puts("Programming is like building a multilingual puzzle \n");
 	return (0);
 }
